723406317938737e963d35eb05852d006fc2398e
sweetybava <66013671+sweetybava@users.noreply.github.com>
2021-12-30 22:33:27 +0530

    Add files via upload

---
+++ Projects/Project02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Project02
+{
+    class Program
+    {
+        //method
+        static Boolean validatePassword(string password)
+        {
+            //using regex to validate the password
+            Regex re = new Regex("^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@#%$^&+=])(?=\\S+$).{8,}$");
+            bool values = re.IsMatch(password);
+            return values;
+
+        }
+
+        //main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the password to be validated:");
+            string password = Console.ReadLine();
+            if (validatePassword(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                Console.WriteLine("Password is invalid");
+            }
+        }
+    }
+}
