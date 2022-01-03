9177f25b0c099d793b634cdecbd22b8275e982fd
sweetybava <66013671+sweetybava@users.noreply.github.com>
2022-01-03 10:14:27 +0530

    Add files via upload

---
+++ Sample programs C#/AbstractionSamplePrg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbstractionSamplePrg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StoryContent sc = new StoryContent();
+
+            sc.storyTitle();
+            sc.storyAuthorName();
+        }
+    }
+}
---
+++ Sample programs C#/AbstractionSamplePrg/Story.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionSamplePrg
+{
+    //abstract class
+    abstract class Story
+    {
+        //abstract method - does not have the body
+        public abstract void storyTitle();
+
+        //normal method
+        public void storyAuthorName()
+        {
+            Console.WriteLine("Author Name : Peter Sohn");
+        }
+        
+    }
+
+    //StoryContent class inherit the abstract Story class
+    class StoryContent : Story
+    {
+        //override the abstract method
+        public override void storyTitle()
+        {
+            Console.WriteLine("Title: The Good Dinosaur");
+        }
+    }
+}
