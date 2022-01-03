b9665f6376a51152791ce4f5bae9b2a5abca52c4
sweetybava <66013671+sweetybava@users.noreply.github.com>
2022-01-03 11:52:33 +0530

    Add files via upload

---
+++ Sample programs C#/InterfaceSamplePrg/Interface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSamplePrg
+{
+    //Interface
+    public interface IBooks
+    {
+        //abstract method
+        void bookTitle();
+    }
+
+    //inherit the interface class
+    class BookName : IBooks
+    {
+        //method
+        public void bookTitle()
+        {
+            //get the user input
+            Console.WriteLine("Enter the book name: ");
+            string bookName = Console.ReadLine();
+
+            //Displaying the values
+            Console.WriteLine("Book Name: " + bookName);
+        }
+    }
+
+    //edition class inherit the Ibooks interface
+    class Edition : IBooks
+    {
+        //method
+        public void bookTitle()
+        {
+            //user input
+            Console.WriteLine("Enter the book edition: ");
+            int bookEdition = Convert.ToInt32(Console.ReadLine());
+
+            //Displaying the values
+            Console.WriteLine("Book Name: " + bookEdition);
+        }
+    }
+}
---
+++ Sample programs C#/InterfaceSamplePrg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InterfaceSamplePrg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object 
+            IBooks ib;
+            ib = new BookName();
+            ib.bookTitle();
+            ib = new Edition();
+            ib.bookTitle();
+
+            
+        }
+    }
+}
